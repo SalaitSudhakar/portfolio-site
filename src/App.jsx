d1881620cd8041438e7663801be61bf4b9e0298b
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-04-22 22:05:57 +0530

    Add loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Componnets/Navbar";
 import Hero from "./Componnets/Hero";
 import "./App.css";
 import About from "./Componnets/About";
 import Skills from "./Componnets/Skills";
-import Contact from './Componnets/Contact';
-import Footer from './Componnets/Footer'
+import Contact from "./Componnets/Contact";
+import Footer from "./Componnets/Footer";
 import MoveToTop from "./Componnets/MoveToTop";
 import Projects from "./Componnets/Projects";
+import { HashLoader } from "react-spinners";
 
 const App = () => {
-   const [activeSection, setActiveSection] = useState("home");
+  const [activeSection, setActiveSection] = useState("home");
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  /* Loading section */
+  if (isLoading) {
+    return (
+      <div className="absolute inset-0 flex justify-center items-center bg-[#191c36]">
+        <HashLoader color="#14b8a6" size={100} />
+      </div>
+    );
+  }
   return (
     <div className="relative bg-custom-dark-blue min-h-screen text-white overflow-x-hidden">
-      <Navbar activeSection={activeSection} setActiveSection={setActiveSection}/>
-      <MoveToTop activeSection={activeSection}/>
+      <Navbar
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+      />
+      <MoveToTop activeSection={activeSection} />
       <Hero />
       <About />
       <Skills />
